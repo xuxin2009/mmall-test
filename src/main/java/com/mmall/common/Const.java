bd93f0217e9fd266a3ca56022ce17052c3d80a29
xuxin2009 <724056979@qq.com>
2018-09-19 16:59:02 +0800

    add ProductController ... files

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -1,5 +1,9 @@
 package com.mmall.common;
 
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
 /**
  * Created by Administrator on 2018/9/12.
  */
@@ -8,9 +12,31 @@ public class Const {
     public static  final String EMAIL = "email";
     public static final  String USERNAME = "userName";
 
+    public interface productListOrderBy{
+        Set<String> PRICE_DESC_ASC = Sets.newHashSet("price_desc","price_asc");
+    }
     public interface Role
     {
         int ROLE_CUSTOMER = 0;//普通用户
         int ROLE_ADMIN = 1;//admin
+    }
+    public enum ProductStatus
+    {
+        ON_SALE(1,"在线");
+        private String value;
+        private int code;
+
+        ProductStatus( int code,String value) {
+            this.value = value;
+            this.code = code;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public int getCode() {
+            return code;
+        }
     }
 }
--- src/main/java/com/mmall/dao/ProductMapper.java
+++ src/main/java/com/mmall/dao/ProductMapper.java
@@ -1,6 +1,9 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.Product;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface ProductMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +17,13 @@ public interface ProductMapper {
     int updateByPrimaryKeySelective(Product record);
 
     int updateByPrimaryKey(Product record);
+
+    int updateSetProductStatus(@Param("productId") Integer productId,@Param("status") Integer status);
+
+    List<Product> selectProductList();
+
+    List<Product> selectSearchProductByIdOrName(@Param("productName") String productName,@Param("productId") int productId);
+
+    List<Product> selectByNameAndCategoryId(@Param("productName") String productName,
+                                            @Param("categoryList") List<Integer>categoryList);
 }
